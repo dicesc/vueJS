84f5f213636f05868c25ca42ff649c9e834cbbe2
diaochuan <dicesc@163.com>
2018-08-27 16:01:51 +0800

    first commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div >
+    <li @click="clickDelete">{{content}}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['content','index'],
+  methods: {
+      clickDelete () {
+          this.$emit('delete',this.index)
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
