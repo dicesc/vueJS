a2287db532bd9c77b285487d23fe9859db816417
diaochuan <dicesc@163.com>
2018-08-29 16:10:50 +0800

    add weekend on home page

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -1,14 +1,81 @@
 <template>
-    <div>
-
+  <div>
+    <p class="recommend-title">热门推荐</p>
+    <div class="list" v-for="item in lists" :key="item.id">
+      <img class="left-img" :src="item.url" />
+      <div class="content">
+        <p class="title">{{item.title}}</p>
+        <p class="desc">{{item.desc}}</p>
+        <button class="button">查看详情</button>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeRecommend'
+  name: 'HomeRecommend',
+  data () {
+    return {
+      lists: [
+        {id: '0001', url: 'http://img1.qunarzz.com/sight/p0/1505/be/be4802e10f3b3107.water.jpg_200x200_0e98aabe.jpg', title: '就是这个了', desc: '这里是个很好玩的地方哦'},
+        {id: '0002', url: 'http://img1.qunarzz.com/sight/p0/1505/be/be4802e10f3b3107.water.jpg_200x200_0e98aabe.jpg', title: '重庆必游TOP10', desc: '重庆, 非去不可'},
+        {id: '0003', url: 'http://img1.qunarzz.com/sight/p0/1505/be/be4802e10f3b3107.water.jpg_200x200_0e98aabe.jpg', title: '就是这个了', desc: '这里是个很好玩的地方哦'}
+      ]
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+
+.recommend-title {
+  background: #eeeeee;
+  line-height: 0.6rem;
+  padding-left: 0.15rem;
+}
+
+.list {
+  background: #ffffff;
+  display: flex;
+
+  .left-img {
+    width: 1.6rem;
+    height: 1.6rem;
+    margin: 0.2rem;
+  }
+
+  .content {
+    flex: 1;
+    min-width: 0;
+
+    .title {
+      font-size: 0.32rem;
+      color: @212121;
+      line-height: 0.44rem;
+      margin-top: 0.26rem;
+      ellipsis();
+    }
+
+    .desc {
+      font-size: 0.26rem;
+      color: #cccccc;
+      line-height: 0.3rem;
+      margin-top: 0.1rem;
+      ellipsis();
+    }
+
+    .button {
+      background: #ed8f00;
+      color: #fff;
+      font-size: 0.26rem;
+      line-height: 0.4rem;
+      margin-top: 0.2rem;
+      padding-left: 0.15rem;
+      padding-right: 0.15rem;
+      border-radius: 0.05rem;
+    }
+  }
+}
 </style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <p class="recommend-title">周末去哪儿</p>
+        <ul>
+            <li class="list" v-for="item in lists" :key="item.id">
+                <div class="top-img-wapper">
+                    <img class="top-img" :src="item.url" />
+                </div>
+                <div class="content">
+                    <p class="title">{{item.title}}</p>
+                    <p class="desc">{{item.desc}}</p>
+                </div>
+
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      lists: [
+        {id: '0001', url: 'http://img1.qunarzz.com/sight/source/1505/5f/276ed6784d25ee.jpg_r_640x214_2193082e.jpg', title: '就是这个了', desc: '这里是个很好玩的地方哦'},
+        {id: '0002', url: 'http://img1.qunarzz.com/sight/source/1505/5f/276ed6784d25ee.jpg_r_640x214_2193082e.jpg', title: '重庆必游TOP10', desc: '重庆, 非去不可'},
+        {id: '0003', url: 'http://img1.qunarzz.com/sight/source/1505/5f/276ed6784d25ee.jpg_r_640x214_2193082e.jpg', title: '就是这个了', desc: '这里是个很好玩的地方哦'},
+        {id: '0004', url: 'http://img1.qunarzz.com/sight/source/1507/8e/f41005646c1e8e.jpg_r_640x214_65f25f65.jpg', title: '就是这个了', desc: '这里是个很好玩的地方哦'},
+        {id: '0005', url: 'http://img1.qunarzz.com/sight/p0/201305/08/8638c7dbd656cafac8d65eac.jpg_600x330_af87cf27.jpg', title: '就是这个了', desc: '这里是个很好玩的地方哦'}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl';
+
+.recommend-title {
+    background: #eeeeee;
+    line-height: 0.6rem;
+    padding-left: 0.15rem;
+}
+
+.list {
+    background: #ffffff;
+
+    .top-img-wapper {
+        height: 0;
+        overflow: hidden;
+        padding-bottom: 33.9%;
+
+        .top-img {
+            width: 100%;
+        }
+    }
+
+    .content {
+        padding: 0.1rem;
+
+        .title {
+            font-size: 0.32rem;
+            color: @212121;
+            line-height: 0.44rem;
+            margin-top: 0.1rem;
+            ellipsis();
+        }
+
+        .desc {
+            font-size: 0.26rem;
+            color: #cccccc;
+            line-height: 0.4rem;
+            margin-top: 0.1rem;
+            ellipsis();
+        }
+    }
+}
+</style>
