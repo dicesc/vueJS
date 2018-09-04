100cac12a37c468dcba510ee4891ee1a432d3599
diaochuan <dicesc@163.com>
2018-09-04 15:15:47 +0800

    性能优化 防止多次请求数据

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,16 +15,21 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 
 export default {
   name: 'Home',
   data: function () {
     return {
+      lastCity: '',
       swiperList: [],
       iconsList: [],
       recommendList: [],
       weekendList: []
     }
+  },
+  computed: {
+    ...mapState(['city'])
   },
   components: {
     HomeHeader,
@@ -35,7 +40,7 @@ export default {
   },
   methods: {
     getHomeInfo () {
-      axios.get('/api/index.json').then(this.getHomeInfoSucc)
+      axios.get('/api/index.json?city' + this.city).then(this.getHomeInfoSucc)
     },
     getHomeInfoSucc (e) {
       if (e.data.ret && e.data.data) {
@@ -49,6 +54,12 @@ export default {
   },
   mounted () {
     this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.getHomeInfo()
+      this.lastCity = this.city
+    }
   }
 }
 </script>
